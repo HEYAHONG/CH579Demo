7b2c87ba427e50fb2371a5e0b891d9304bea0f6b
何亚红 <2229388563@qq.com>
2021-10-28 10:38:02 +0800

    以太网初步测试通过

--- Main.c
+++ Main.c
@@ -17,9 +17,10 @@ void main_task1(void *arg)
   }
 }
 
+extern int net_main(void);
+
 void main_task2(void *arg)
 {
- 
   while(1)
   {
      vTaskDelay(5);
@@ -27,6 +28,8 @@ void main_task2(void *arg)
 }
 
 extern void HardwareInit(void);
+
+void  net_main_task(void *arg);//网络主任务
 
 int main()
 {  
@@ -37,6 +40,7 @@ int main()
 		xTaskCreate( main_task1, "main_task1",128, NULL, 1, NULL );
 		xTaskCreate( main_task2, "main_task2",128, NULL, 1, NULL );
 	
+		xTaskCreate( net_main_task, "net_main_task",512, NULL, 1, NULL );//网络主任务占用堆栈较大	
 	
 		FreeRTOS_Start();//启动FreeRTOS（更新时钟，启用调度器）
     while(1);    
@@ -44,5 +48,21 @@ int main()
 
 void HardwareInit(void)
 {
-	SetSysClock(CLK_SOURCE_HSI_32MHz);
+	PWR_UnitModCfg(ENABLE, UNIT_SYS_PLL);                                      /* PLL上电 */
+	
+	SetSysClock(CLK_SOURCE_HSE_32MHz);                                          /* 外部晶振 PLL 输出32MHz */
+	//SetSysClock(CLK_SOURCE_HSI_32MHz);//初始化时钟
+	
+	{
+		//初始化串口,printf打印输出
+		GPIOA_SetBits( GPIO_Pin_9 );
+		GPIOA_ModeCfg( GPIO_Pin_9, GPIO_ModeOut_PP_5mA );                           /* 串口1的IO口设置 */
+		UART1_DefInit(); //初始化UART1,默认打印
+	}
+	
+}
+
+void HardFault_Handler(void)
+{
+	while(1);
 }
--- ReadMe.md
+++ ReadMe.md
@@ -8,6 +8,12 @@
 - lib/CH579EVT:官方公开的库目录。
 - lib/FreeRTOS:FreeRTOS源代码及配置文件。FreeRTOSConfig.h为FreeRTOS的配置文件，可根据自己需要定制各种选项。
 
+## 功能说明
+
+原官方的TCP/IP库为无OS下的网络库,修改为任务后通过DHCP与Ping测试,但新加功能时一定要注意多任务环境下同步问题。
+
+
+
 # 编译条件
 
 - 安装好keil5环境。
@@ -16,3 +22,9 @@
 # 调试
 
 可采用jlink直接调试，具体设置及问题参考lib/CH579EVT/中的例子。
+
+## 串口输出
+
+printf的内容将由串口输出。
+
+UART1：115200 8N1
